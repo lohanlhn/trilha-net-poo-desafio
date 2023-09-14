8b37b5215e28d203d132fe58171665cf117e75b2
lohanlhn <lohan.massote@gmail.com>
2023-09-14 17:31:00 -0300

    desafio concluido

--- Program.cs
+++ Program.cs
@@ -1,3 +1,13 @@
 ﻿using DesafioPOO.Models;
 
-// TODO: Realizar os testes com as classes Nokia e Iphone
+Console.WriteLine("Celular Nokia");
+var nokia = new Nokia("123456", "modelo 1", "6542318", 32);
+nokia.Ligar();
+nokia.InstalarAplicativo("Telegram");
+
+Console.WriteLine("\n\n\n");
+
+Console.WriteLine("Celular Iphone");
+var iphone = new Iphone("456123", "modelo 2", "587984", 64);
+iphone.ReceberLigacao();
+iphone.InstalarAplicativo("Linkedin");
